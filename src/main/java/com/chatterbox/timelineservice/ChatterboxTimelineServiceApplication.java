b86b478d15142f181c2b9b2363295542a427c5b9
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-04 19:04:05 +0530

    Integrate Kafka consumer for new Post events

--- src/main/java/com/chatterbox/timelineservice/ChatterboxTimelineServiceApplication.java
+++ src/main/java/com/chatterbox/timelineservice/ChatterboxTimelineServiceApplication.java
@@ -2,8 +2,10 @@ package com.chatterbox.timelineservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ChatterboxTimelineServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatterboxTimelineServiceApplication.class, args);
--- src/main/java/com/chatterbox/timelineservice/service/TimelineService.java
+++ src/main/java/com/chatterbox/timelineservice/service/TimelineService.java
@@ -10,7 +10,12 @@ import java.util.List;
 public class TimelineService {
 
     public List<Post> getUserTimeline(String userId) {
+        // TODO:
+        // invoke follower service get userId's following and generate feed of top 20 post
+        // 20 could be configurable?
         // Mock posts
+        // Communicates with Follower Service to get list of followed users
+        //Queries Post Service for recent posts from followed users
         return List.of(
                 new Post("p1", userId, "First post!", Instant.now()),
                 new Post("p2", userId, "Second post!", Instant.now())
