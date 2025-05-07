c58ce52d33f42e1fba195e05a7a52bf397262525
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-08 00:53:26 +0530

    Convert class to record

--- src/main/java/com/chatterbox/timelineservice/model/Post.java
+++ src/main/java/com/chatterbox/timelineservice/model/Post.java
@@ -1,23 +1,5 @@
 package com.chatterbox.timelineservice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.time.Instant;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-public class Post {
-    private String postId;
-    private String username;
-    private String content;
-    private Instant timestamp;
-}
+public record Post(String postId, String username, String content, Instant timestamp) {}
